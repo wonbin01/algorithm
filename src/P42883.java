8b55c81371664fbe66259e34e0fc29abb62ee0a4
wonbin <wonbin3977@gmail.com>
2026-02-19 15:19:46 +0900

    P42883

---
+++ src/P42883.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String number, int k) {
+        StringBuilder sb = new StringBuilder();
+        int remove = k;
+        for (int i = 0; i < number.length(); i++) {
+            char c = number.charAt(i);
+            while (sb.length() > 0 
+                   && remove > 0 
+                   && sb.charAt(sb.length() - 1) < c) {
+                sb.deleteCharAt(sb.length() - 1);
+                remove--;
+            }
+            sb.append(c);
+        }
+        // 아직 제거 못한 게 있으면 뒤에서 제거
+        return sb.substring(0, sb.length() - remove);
+    }
+}
