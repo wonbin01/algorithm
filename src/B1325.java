ea5d28ef6f46bd55414cb8c085d4230ac4d22a38
wonbin <wonbin3977@gmail.com>
2026-02-23 15:46:04 +0900

    B1325

--- src/B1325.java
+++ src/B1325.java
@@ -1,74 +1,56 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 
-public class B1325
-{
-    static HashMap<Integer, Integer> key = new HashMap<>();
-    
-    public static void main(String args[]) throws IOException 
-    {
+public class B1325 {
+    static ArrayList<Integer>[] graph;
+    static int maxValue = 0;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
-        int n = Integer.parseInt(input[0]); // n개의 노드
-        int m = Integer.parseInt(input[1]); // m개의 간선
-        HashMap<Integer, ArrayList<Integer>> hm = new HashMap<>();
-
-        for (int i = 1; i <= n; i++) 
-        {
-            hm.put(i, new ArrayList<>());
+        int n = Integer.parseInt(input[0]); // 컴퓨터의 개수
+        int m = Integer.parseInt(input[1]); // 관계의 개수
+        graph = new ArrayList[n+1];
+        for(int i=1;i<=n;i++) {
+            graph[i] = new ArrayList<>();
         }
-
-        for (int i = 0; i < m; i++) 
-        {
+        for(int i=0;i<m;i++) {
             input = br.readLine().split(" ");
-            int hacked = Integer.parseInt(input[0]); // 목적지 노드
-            int target = Integer.parseInt(input[1]); // 출발 노드
-            hm.get(target).add(hacked); // 방향성 있는 간선
+            int start = Integer.parseInt(input[0]);
+            int end = Integer.parseInt(input[1]);
+            graph[end].add(start);
         }
-
-        for (int i = 1; i <= n; i++) 
-        {
-            bfs(i, hm);
+        for(int i=1;i<=n;i++) {
+            hacking(i,n);
         }
-
-        int maxHacked = Collections.max(key.values());
-        ArrayList<Integer> result=new ArrayList<>();
-        for (int node : key.keySet()) 
-        {
-            if (key.get(node) == maxHacked) 
-            {
-                result.add(node);
-            }
-        }
-        Collections.sort(result);
-
-        for(int node : result)
-        {
-            System.out.print(node + " ");
-        }
+        System.out.println(sb);
     }
-
-    public static void bfs(int start, HashMap<Integer, ArrayList<Integer>> hm) 
-    {
-        Queue<Integer> queue=new LinkedList<>();
-        boolean[] visited = new boolean[hm.size() + 1]; // 1-based index 대응
-        queue.add(start);
-        visited[start] = true;
-        int count = 1;
-
-        while (!queue.isEmpty()) 
-        {
-            int node = queue.poll();
-            for (int neighbor : hm.get(node)) 
-            {
-                if (!visited[neighbor]) 
-                {
-                    visited[neighbor] = true;
-                    queue.add(neighbor);
-                    count++;
+    static void hacking(int startNode,int n) {
+        boolean[] visited = new boolean[n+1];
+        int cnt=1;
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        stack.add(startNode);
+        visited[startNode]=true;
+        while(!stack.isEmpty()) {
+            int cx = stack.pop();
+            for(int nx : graph[cx]) {
+                if(!visited[nx]) {
+                    visited[nx]=true;
+                    cnt++;
+                    stack.add(nx);
                 }
             }
         }
-        key.put(start, count);
+        if(maxValue==cnt) {
+            sb.append(startNode).append(" ");
+        }
+        else if(maxValue<cnt) {
+            maxValue = cnt;
+            sb = new StringBuilder();
+            sb.append(startNode).append(" ");
+        }
     }
 }
