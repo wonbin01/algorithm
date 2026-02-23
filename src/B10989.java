9be8c22deb83fb6edbc7742a5a5c76dcbeba425c
wonbin <wonbin3977@gmail.com>
2026-02-23 10:46:46 +0900

    B10989

---
+++ src/B10989.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class B10989 {
+    static StringBuilder sb =new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        for(int num : arr) {
+            sb.append(num).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
