c9351efc3ce1306e9c1230cc37c3d63ba300a5ee
wonbin <wonbin3977@gmail.com>
2026-02-23 13:31:17 +0900

    B11650

---
+++ src/B11650.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class B11650 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        ArrayList<int[]> list = new ArrayList<>();
+        for(int i=0;i<n;i++) {
+            String[] input = br.readLine().split(" ");
+            int x=Integer.parseInt(input[0]);
+            int y=Integer.parseInt(input[1]);
+            list.add(new int[] {x,y});
+        }
+        Collections.sort(list,(a,b) -> {
+            if(a[0]==b[0]) {
+                return Integer.compare(a[1],b[1]);
+            }
+            return Integer.compare(a[0],b[0]);
+        });
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<list.size();i++) {
+            int[] current = list.get(i);
+            sb.append(current[0]).append(" ").append(current[1]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
