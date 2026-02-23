4df9471b6e80f20e739e94399b84c7eb9294ec90
wonbin <wonbin3977@gmail.com>
2026-02-23 14:20:19 +0900

    B10870

---
+++ src/B10870.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B10870 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        if(n==0) {
+            System.out.println(0);
+            return;
+        }
+        else if(n==1) {
+            System.out.println(1);
+            return;
+        }
+        int[] dp = new int[n+1];
+        dp[0]=0; dp[1]=1;
+        for(int i=2;i<=n;i++) {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        System.out.println(dp[n]);
+    }
+}
