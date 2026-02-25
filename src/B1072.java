389cc0c8b225b4d351b95ceaf5293bf2da4e27da
wonbin <wonbin3977@gmail.com>
2026-02-25 14:27:41 +0900

    B1072

--- src/B1072.java
+++ src/B1072.java
@@ -6,28 +6,34 @@ public class B1072 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
-        long total = Long.parseLong(input[0]); // 전체 게임횟수
-        long winNumber = Long.parseLong(input[1]); // 이긴 횟수
-        long possibility = (int)((double)winNumber / total * 100);
-        if(possibility>=99) { // 확률이 절대 변하지 않음
+
+        long total = Long.parseLong(input[0]);
+        long winNumber = Long.parseLong(input[1]);
+
+        long possibility = (winNumber * 100) / total;
+
+        if (possibility >= 99) {
             System.out.println(-1);
             return;
         }
-        long left=1;
+
+        long left = 1;
         long right = 1000000000;
-        long mid = -1;
-        long answer=0;
-        while(left <= right) {
-            mid = (left+right)/2;
-            long current = ((winNumber+mid)*100) / (total+mid);
-            if(current>possibility) {
+        long answer = 0;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+
+            long current = ((winNumber + mid) * 100) / (total + mid);
+
+            if (current > possibility) {
                 answer = mid;
-                right = mid-1;
-            }
-            else {
-                left = mid+1;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
+
         System.out.println(answer);
     }
 }
